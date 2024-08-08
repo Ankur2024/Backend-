17b7cb69f5abee437ee533f60c40ea9b1a1d58cf
Ankur2024 <ag3942280@gmail.com>
2024-08-08 16:06:08 +0530

    Learned HTTP Method

--- Backend_4/server/index.js
+++ Backend_4/server/index.js
@@ -1,14 +1,39 @@
 const http = require("http");
 const fs = require('fs');
+const url = require("url"); // Correct the import here
 
 // it will create a web server
 const myServer = http.createServer((req, res) => {
-    // console.log(req);
-    const log = `${Date.now()}: new req received\n`;
-    fs.appendFile("log.txt", log, (err, data)=> {
-    res.end("Hello from server");
-
-    })
+    if(req.url === '/favicon.ico') return res.end();
+    const log = `${Date.now()}: ${req.method} ${req.url} new req received\n`;
+    const myUrl = url.parse(req.url, true); 
+    console.log(myUrl);
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) throw err; 
+        switch (myUrl.pathname) {
+            case "/":
+                if(req.method === 'GET') res.end("Home");
+                break;
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end(`Search results for ${search}`);
+                break;
+            case "/signup":
+                if(req.method === "GET") res.end("This is a sign form");
+                else if(req.method === "POST"){
+                    //DB Query
+                    res.end("Sucess");
+                }
+                break;
+            default:
+                res.end("404 Not Found !!");
+                break;
+        }
+    });
 });
 
 myServer.listen(8000, () => console.log("Server Started"));
